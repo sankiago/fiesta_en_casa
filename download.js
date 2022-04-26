52e06816f1ee1ad0ff853460be6666d1b2bd82ef
sankiago <69083739+sankiago@users.noreply.github.com>
2022-04-25 20:42:15 -0500

    Add files via upload
    initial commit

---
+++ download.js
@@ -0,0 +1,22 @@
+const downloadAudio = async (path, filename) => {
+    const response = await fetch(path)
+
+
+    const blob = await response.blob()
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.style.display = 'none';
+    a.href = url;
+    // the filename you want
+    a.download = filename;
+    document.body.appendChild(a);
+    a.click();
+    window.URL.revokeObjectURL(url);
+    console.log(`${filename} se está descargando`);
+}
+
+const numeroDeCanciones = 30
+
+for(let i=0; i<numeroDeCanciones; i++){
+    downloadAudio(`./audio/${i+1}.m4a`,i+1)
+}
